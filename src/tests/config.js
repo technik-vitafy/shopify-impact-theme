340f988dbeabeab6c3afad263382ce29faa47547
Edo Monticelli <e.monticelli@vitafybrands.de>
2024-11-14 23:18:20 +0100

    Fix Vitafy font; dynamic component mock data

--- src/tests/config.js
+++ src/tests/config.js
@@ -3,6 +3,22 @@ export const stores = {
         homepage: 'https://vitafy-ii.myshopify.com/',
         cssThemeUrl: 'https://vitafy-ii.myshopify.com/cdn/shop/t/3/assets/theme.css',
         jsThemeUrl: 'https://vitafy-ii.myshopify.com/cdn/shop/t/3/assets/theme.js',
-        settingsData: 'https://raw.githubusercontent.com/technik-vitafy/shopify-impact-theme/refs/heads/main/config/settings_data.json'
+        settingsData: 'https://raw.githubusercontent.com/technik-vitafy/shopify-impact-theme/refs/heads/main/config/settings_data.json',
+        fonts: {
+            MuseoSans: [
+                {
+                    url: 'https://vitafy-ii.myshopify.com/cdn/shop/files/museo_sans_regular.woff2',
+                    format: 'woff2',
+                    weight: 400,
+                    style: 'normal',
+                },
+                {
+                    url: 'https://vitafy-ii.myshopify.com/cdn/shop/files/museo_sans_bold.woff2',
+                    format: 'woff2',
+                    weight: 700,
+                    style: 'normal',
+                },
+            ],
+        }
     }
 }
